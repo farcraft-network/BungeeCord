a5f77b9f7a541b8760716fa6cde2bfa5f2943dac
Kacy Luzzardi <kacymimo@europe.com>
2017-12-30 06:58:59 +0100

    Update and rename CommandBungee.java to CommandFarCord.java

--- proxy/src/main/java/net/md_5/bungee/command/CommandFarCord.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandFarCord.java
@@ -5,17 +5,17 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class CommandBungee extends Command
+public class CommandFarCord extends Command
 {
 
-    public CommandBungee()
+    public CommandFarCord()
     {
-        super( "bungee" );
+        super( "farcord" );
     }
 
     @Override
     public void execute(CommandSender sender, String[] args)
     {
-        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
+        sender.sendMessage( ChatColor.BLUE + "Ce serveur est propulsé par FarCord " + ProxyServer.getInstance().getVersion() );
     }
 }
